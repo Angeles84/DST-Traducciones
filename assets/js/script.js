7f51c4da45aff5bc3bc4f8a3c302e96600ba92ec
Angeles <angie.oliva@gmail.com>
2021-06-29 10:43:08 -0400

    agrandando carrusel

---
+++ assets/js/script.js
@@ -0,0 +1,12 @@
+/* Fix iOS 7 bug for -unit support */
+var iOS = navigator.userAgent.match(/(iPod|iPhone|iPad)/);
+
+if(iOS){
+    function iosVhHeightBug() {
+        var height = $(window).height();
+        $("#article-why").css('height', height);
+    }
+ 
+    iosVhHeightBug();
+    $(window).bind('resize', iosVhHeightBug);
+}
