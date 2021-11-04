1d3df12b5b07b5842bd0fae7ba25d3091bb11c67
Angeles <angie.oliva@gmail.com>
2021-11-04 14:57:15 -0300

    antes de termiar

--- assets/js/script.js
+++ assets/js/script.js
@@ -62,11 +62,68 @@ function validar(nombre, correo, mensaje) {
 }
 
 /**
- function validar(nombre, correo, mensaje) {
-  let pasamosLaValidacion = true;
-  let validacionNombre = /^[a-zA-ZÀ-ÿ\u00f1\u00d1]+(\s*[a-zA-ZÀ-ÿ\u00f1\u00d1]*)*[a-zA-ZÀ-ÿ\u00f1\u00d1]+$/g;
+ "use strict";
+let archivo = document.querySelector("#archivo");
+archivo.addEventListener("change" , () => {
+  document.querySelector("#placeholder").innerHTML = archivo.files[0].name;
+})
 
-  if (validacionNombre.test(nombre) == false) {
+//validación del formulario
+
+let formulario = document.getElementById("formulario");
+
+formulario.addEventListener("submit", async function (event) {
+  event.preventDefault();
+  limpiarErrores();
+
+  let textNombre = document.getElementById("nombre").value;
+  let textCorreo = document.getElementById("email").value;
+  let textMensaje = document.getElementById("mensaje").value;
+  let textArchivo = document.getElementById("archivo").value;
+  let resultado = validar(textNombre, textCorreo, textMensaje);
+  let modal = document.getElementById("modal");
+
+  if (resultado) {
+    try {
+      const formData = new FormData();
+      formData.append("nombre", textNombre);
+      formData.append("email", textCorreo);
+      formData.append("mensaje", textMensaje);
+      formData.append("archivo" , textArchivo);
+      await fetch("/contacto-api.php",{
+        method: "POST",
+        body: formData
+      })
+      limpiarInputs();
+      $(modal).modal("show");
+    } catch (error) {
+      console.error(error)
+    }
+   
+  } else { 
+    $(modal).modal("hide");
+  }
+});
+
+function limpiarInputs() {
+  const inputs = document.querySelectorAll("input");
+  const textArea = document.querySelectorAll("textarea");
+
+  inputs.forEach((input) => (input.value = ""));
+  textArea.forEach((textarea) => (textarea.value = ""));
+  document.querySelector("#placeholder").innerHTML = 'Ningún archivo seleccionado';
+}
+
+function limpiarErrores() {
+  document.querySelector(".errorNombre").innerHTML = "";
+  document.querySelector(".errorCorreo").innerHTML = "";
+  document.querySelector(".errorMensaje").innerHTML = "";
+}
+
+function validar(nombre, correo, mensaje) {
+  let pasamosLaValidacion = true;
+ 
+  if (nombre == "") {
     document.querySelector(".errorNombre").innerHTML = "El nombre es requerido.";
     pasamosLaValidacion = false;
   }
@@ -77,11 +134,16 @@ function validar(nombre, correo, mensaje) {
     pasamosLaValidacion = false;
   }
 
-  let validacionMensaje = /^[a-zA-ZÀ-ÿ0-9\u00f1\u00d1]+(\s*[a-zA-ZÀ-ÿ0-9\u00f1\u00d1]*)*[a-zA-ZÀ-ÿ0-9\u00f1\u00d1]+$/g; 
-  if (validacionMensaje.test(mensaje) == false) {
+  if (mensaje == "") {
     document.querySelector(".errorMensaje").innerHTML = "El mensaje es requerido.";
     pasamosLaValidacion = false;
   }
   return pasamosLaValidacion;
 }
- */
+
+/*
+scrollBehavior() {
+    return { x: 0, y: 0 };
+  },
+*/
+ 
